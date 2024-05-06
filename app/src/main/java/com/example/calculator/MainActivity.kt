2671e617752a5e224db45dabf06de8d874a3a348
Matias <matias_r12@hotmail.com>
2024-05-06 11:25:03 -0300

    Funcionalidad al boton delete

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -6,8 +6,6 @@ import android.view.View
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.calculator.databinding.ActivityMainBinding
 
 
@@ -21,9 +19,27 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    //ESTE METODO PUBLICO ENVIA PROPIEDAD ON CLICK A LOS XML
+    //ESTE METODO PUBLICO ENVIA PROPIEDAD ONCLICK A LOS XML
     fun onClickButton(view: View){
-        Log.i("click", (view as Button).text.toString())
+        val valueStr = (view as Button).text.toString()
+    //ESTO VA A EVALIAR LA VISTA QUE CORRESPONDE AL BOTON PULSADO
+        when(view.id){
+            R.id.btnDelete -> { //RECORDAR QUE LA FLECHA ES PARA ABRIR UN NUEVO BLOQUE DE CODIGO
+                val length = binding.tvOperation.text.length //AVERIGUA LA LONGITUD ACTUAL DE LA OPERACION
+                val newOperation = binding.tvOperation.text.toString().substring(0, length -1)
+                binding.tvOperation.text = newOperation //asignamos newOperation y elimina el ultimo caracter
+            }
+            R.id.btnClear -> { //RECORDAR QUE LA FLECHA ES PARA ABRIR UN NUEVO BLOQUE DE CODIGO
+
+            }
+            R.id.btnResult -> { //RECORDAR QUE LA FLECHA ES PARA ABRIR UN NUEVO BLOQUE DE CODIGO
+
+            }
+            //else ESTA PARA QUE IMPRIMA LOS TXT OSEA LOS NUMEROS
+            else -> { //RECORDAR QUE LA FLECHA ES PARA ABRIR UN NUEVO BLOQUE DE CODIGO
+                binding.tvOperation.append(valueStr) //append ES UN METODO QUE AÑADE TEXTO AL FINAL EL NUEVO VALOR
+            }
+        }
     }
 }
 
