e57968eb46f6ecb164319ebf9261cf88d6942b5a
Matias <matias_r12@hotmail.com>
2024-05-07 16:02:01 -0300

    Se creo una inteface para comunicar las clases

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -52,7 +52,7 @@ class MainActivity : AppCompatActivity() {
                 binding.tvResult.text = ""
             }
             //RECORDAR QUE LA FLECHA ES PARA ABRIR UN NUEVO BLOQUE DE CODIGO
-            R.id.btnResult ->  tryResolve(operation, true)
+            R.id.btnResult ->  checkOrResolve(operation, true)
 
             //EL CONJUNTO DE BOTONES VAN A DETONAR UN MISMO CODIGO
             R.id.btnMulti,
@@ -60,11 +60,10 @@ class MainActivity : AppCompatActivity() {
             R.id.btnSum,
             R.id.btnSub -> {
                 //el argumento es la operacion previa resuelta
-                tryResolve(operation, false)
+                checkOrResolve(operation, false)
 
                 //AGREGAR OPERADOR EN CASO DE SER VALIDO
-                val operator = valueStr
-                addOperator(operator, operation) //esta funcion recibe argumentos de val
+                addOperator(valueStr, operation) //esta funcion recibe argumentos de val
             }
             //validar si se puede agregar punto en un metodo
             R.id.btnPoint -> addPoint(valueStr, operation)
@@ -116,6 +115,28 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun checkOrResolve(operation: String, isFromResult: Boolean){
+        Operaciones.tryResolve(operation, isFromResult, object : OnResolveListener{
+            override fun onShowResult(result: Double) {
+                //ASIGANA EL RESULTADO
+                binding.tvResult.text = result.toString()
+
+                //COMPROBAMOS SI SE PUEDE COPIAR
+                //si no esta vacio   y   si no viene de result
+                if (binding.tvResult.text.isNotEmpty() && !isFromResult){
+                    binding.tvOperation.text = binding.tvResult.text
+                }
+            }
+            override fun onShowMessage(errorRes: Int) {
+                showMessage()
+            }
+        })
+
+        showMessage()
+
+        showMessage()
+    }
+
     /*ESTE METODO AYUDA A VALIDAR LOS CASOS DONDE ES POSIBLE AÑADIR EL OPERADOR QUE INTENTAMOS AGREGAR
     A LA OPERACION ACTUAL*/
     private fun addOperator(operator: String, operation: String) {
@@ -133,78 +154,6 @@ class MainActivity : AppCompatActivity() {
                 }
         }
     }
-
-    private fun tryResolve(operationRef: String, isFromResult: Boolean) {
-        if (operationRef.isEmpty()){ //si esta vacio retorna para no ejecutar de nuevo
-            return
-        }
-
-        var operation = operationRef //HACEMOS ESTO POR SI HAY UN PUNTO AL FINAL QUITARLO
-        //quita el punto que estes ubicado al final de un valor
-        if (operation.contains(Constantes.POINT) && operation.lastIndexOf(Constantes.POINT) == operation.length -1) {
-            operation = operation.substring(0, operation.length -1)
-        }
-
-        //extrare el operador +,-,*,/
-        val operator = Operaciones.getOperator(operationRef)
-
-        var value = arrayOfNulls<String>(0)
-        //si el operador es diferente de null vamos a descartar que sea una operacion invalida
-        //es decir que no contenga un operador
-        if (operator != Constantes.OPERATOR_NULL){
-            //verificamos si es un operador de resta
-            if (operator == Constantes.OPERATOR_SUB){
-                //extraer el incice del operador -
-                val  index = operation.lastIndexOf(Constantes.OPERATOR_SUB)
-                if (index < operation.length-1) { //si index es menor puede dividirse en 2 si no esta incompleta
-                    value = arrayOfNulls(2)
-                    value[0] = operation.substring(0, index) //index es la posicion de -
-                    value[1] = operation.substring(index+1)
-                }else {
-                    value = arrayOfNulls(1)
-                    value[0] = operationRef.substring(0, index)
-                }
-            }else {
-                value = operation.split(operator).toTypedArray()
-            }
-        }
-
-        if (value.size > 1) {
-            try {
-                //PROCESAR NUMEROS
-                val numberOne = value[0]!!.toDouble()
-                val numberTwo = value[1]!!.toDouble()
-
-                //ASIGANA EL RESULTADO
-                binding.tvResult.text = getResult(numberOne, operator, numberTwo).toString()
-
-                //COMPROBAMOS SI SE PUEDE COPIAR
-                                  //si no esta vacio   y   si no viene de result
-                if (binding.tvResult.text.isNotEmpty() && !isFromResult){
-                    binding.tvOperation.text = binding.tvResult.text
-                }
-            }catch (e: NumberFormatException){
-                if (isFromResult) showMessage()
-            }
-        }else{
-            if (isFromResult && operator != Constantes.OPERATOR_NULL) showMessage() //SI NO TENGO OPERADORES EN PANTALLA NO NOTIFICA
-        }
-        /*//Snackbar es similar a Toast
-        Snackbar.make(binding.root, "1:$numberOne 2:$numberTwo", Snackbar.LENGTH_SHORT).show()*/
-    }
-
-    private fun getResult(numberOne: Double, operator: String, numberTow: Double): Double {
-        var result = 0.0
-
-        when (operator) {
-            Constantes.OPERATOR_MULTI -> result = numberOne * numberTow
-            Constantes.OPERATOR_DIV -> result = numberOne / numberTow
-            Constantes.OPERATOR_SUM -> result = numberOne + numberTow
-            Constantes.OPERATOR_SUB -> result = numberOne - numberTow
-        }
-        return result
-    }
-
     private fun showMessage(){
         Snackbar.make(binding.root, getString(R.string.message_exp_incorrect), Snackbar.LENGTH_SHORT)
             .setAnchorView(binding.llTop).show() //setAnchorView(binding.llTop) situa el mensaje arriba de los botones
--- app/src/main/java/com/example/calculator/Operaciones.kt
+++ app/src/main/java/com/example/calculator/Operaciones.kt
@@ -31,8 +31,74 @@ class Operaciones {
             return (lasElement == Constantes.OPERATOR_MULTI || lasElement ==
                     Constantes.OPERATOR_DIV || lasElement == Constantes.OPERATOR_SUM) && (
                     penultimate == Constantes.OPERATOR_MULTI || penultimate == Constantes.OPERATOR_DIV ||
-                            penultimate == Constantes.OPERATOR_SUM || penultimate == Constantes.OPERATOR_SUB
-                    )
+                            penultimate == Constantes.OPERATOR_SUM || penultimate == Constantes.OPERATOR_SUB)
+        }
+
+        fun tryResolve(operationRef: String, isFromResult: Boolean, listener: OnResolveListener) {
+            if (operationRef.isEmpty()){ //si esta vacio retorna para no ejecutar de nuevo
+                return
+            }
+
+            var operation = operationRef //HACEMOS ESTO POR SI HAY UN PUNTO AL FINAL QUITARLO
+            //quita el punto que estes ubicado al final de un valor
+            if (operation.contains(Constantes.POINT) && operation.lastIndexOf(Constantes.POINT) == operation.length -1) {
+                operation = operation.substring(0, operation.length -1)
+            }
+
+            //extrare el operador +,-,*,/
+            val operator = getOperator(operationRef)
+
+            var value = arrayOfNulls<String>(0)
+            //si el operador es diferente de null vamos a descartar que sea una operacion invalida
+            //es decir que no contenga un operador
+            if (operator != Constantes.OPERATOR_NULL){
+                //verificamos si es un operador de resta
+                if (operator == Constantes.OPERATOR_SUB){
+                    //extraer el incice del operador -
+                    val  index = operation.lastIndexOf(Constantes.OPERATOR_SUB)
+                    if (index < operation.length-1) { //si index es menor puede dividirse en 2 si no esta incompleta
+                        value = arrayOfNulls(2)
+                        value[0] = operation.substring(0, index) //index es la posicion de -
+                        value[1] = operation.substring(index+1)
+                    }else {
+                        value = arrayOfNulls(1)
+                        value[0] = operationRef.substring(0, index)
+                    }
+                }else {
+                    value = operation.split(operator).toTypedArray()
+                }
+            }
+
+            if (value.size > 1) {
+                try {
+                    //PROCESAR NUMEROS
+                    val numberOne = value[0]!!.toDouble()
+                    val numberTwo = value[1]!!.toDouble()
+
+                    listener.onShowResult(getResult(numberOne, operator, numberTwo))
+                }catch (e: NumberFormatException){
+                    if (isFromResult)listener.onShowMessage(R.string.message_num_incorrect)
+
+                }
+            }else{
+                if (isFromResult && operator != Constantes.OPERATOR_NULL)
+                    listener.onShowMessage(R.string.message_exp_incorrect) //SI NO TENGO OPERADORES EN PANTALLA NO NOTIFICA
+            }
+            /*//Snackbar es similar a Toast
+            Snackbar.make(binding.root, "1:$numberOne 2:$numberTwo", Snackbar.LENGTH_SHORT).show()*/
+        }
+
+
+        fun getResult(numberOne: Double, operator: String, numberTow: Double): Double {
+            var result = 0.0
+
+            when (operator) {
+                Constantes.OPERATOR_MULTI -> result = numberOne * numberTow
+                Constantes.OPERATOR_DIV -> result = numberOne / numberTow
+                Constantes.OPERATOR_SUM -> result = numberOne + numberTow
+                Constantes.OPERATOR_SUB -> result = numberOne - numberTow
+            }
+            return result
         }
     }
 }
